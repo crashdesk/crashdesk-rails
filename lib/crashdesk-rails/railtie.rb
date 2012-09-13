e4644429299b9cc5c090c6ce64a4a8e073763e39
Ladislav Martincik <ladislav.martincik@gmail.com>
2012-09-13 11:55:05 +0200

    Adapting to new log style

--- lib/crashdesk-rails/railtie.rb
+++ lib/crashdesk-rails/railtie.rb
@@ -8,10 +8,10 @@ module CrashdeskRails
         config.project_root     ||= ::Rails.root
       end
 
-      Crashdesk.logger.info 'Crahsdesk initializing.'
+      Crashdesk.log 'Crahsdesk initializing.'
 
       if defined? ::ActionDispatch::DebugExceptions
-        Crashdesk.logger.debug 'Crashdesk integrated via including DebugExceptions module.'
+        Crashdesk.log 'Crashdesk integrated via including DebugExceptions module.'
         # DebugException is middleware inside Rails
         # Would be nice to have our own with nice message rendering.
         ::ActionDispatch::DebugExceptions.send(:include, CrashdeskRails::DebugExceptions)
@@ -19,7 +19,7 @@ module CrashdeskRails
         # Also there's ShowException middleware we could override with much
         # nicer default rendering.
       else
-        Crashdesk.logger.debug 'Crashdesk integrated via middleware.'
+        Crashdesk.log 'Crashdesk integrated via middleware.'
         app.config.middleware.use "CrashdeskRails::Middleware"
       end
     end
