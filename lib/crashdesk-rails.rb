e071d52c4267f9e60249ec604f5d2adb2211c9c1
Ladislav Martincik <ladislav.martincik@gmail.com>
2012-09-19 20:46:16 +0200

    Forgot to remove middleware from main file

--- lib/crashdesk-rails.rb
+++ lib/crashdesk-rails.rb
@@ -3,7 +3,6 @@ $:.unshift File.dirname(__FILE__)
 require 'crashdesk'
 
 require 'crashdesk-rails/debug_exceptions'
-require 'crashdesk-rails/middleware'
 require 'crashdesk-rails/action_controller_context'
 require 'crashdesk-rails/version'
 
